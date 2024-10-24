754391d8199c3e4a339cf2dbcac3bf11b0706eae
khouloudrinez <rinezkhouloud@gmail.com>
2024-10-24 12:51:45 +0100

    test4

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ module.exports = () => {
     };
     try {
         console.log("Mongo URI:", 'mongodb+srv://rinezkhouloud:k20101049@cluster0.kcykgao.mongodb.net/eduzen?retryWrites=true&w=majority&appName=Cluster0'); // Add this line
-        mongoose.connect(process.env.MONGO_URI, connectionParams)
+        mongoose.connect('mongodb+srv://rinezkhouloud:k20101049@cluster0.kcykgao.mongodb.net/eduzen?retryWrites=true&w=majority&appName=Cluster0', connectionParams)
             .then(() => {
                 console.log("Connected to database successfully");
             })
